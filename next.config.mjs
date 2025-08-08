d77be5fac421d036f606f4dc2408ff68c36d7e14
webxloom <sowmya@webxloom.com>
2025-08-08 17:41:57 +0530

    Supabase config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+import dotenv from "dotenv";
+dotenv.config();
+
+const domain =
+  process.env.NEXT_PUBLIC_SUPABASE_DOMAIN || "khqdrkgbcnauihndhmka.supabase.co";
 
 const nextConfig = {
   images: {
-    domains: ["wbzgbxllxmhuamnczgvl.supabase.co"],
+    domains: [domain],
   },
 };
 
